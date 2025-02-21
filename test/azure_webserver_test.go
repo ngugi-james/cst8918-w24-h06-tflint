7e2e66682c5a7d105780220b691ddf1e8fd6917b
ngugi-james <ngugijamesw@gmail.com>
2025-02-21 17:42:20 -0500

    Update the subscriptionID and labelPrefix variables in the test file.

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-subscription-id>"
+var subscriptionID string = "b3dac7bd-3af2-4f39-8244-cff239d62f24"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-username>",
+			"labelPrefix": "wair0001",
 		},
 	}
 
